08c5bb7e79f448fe3ebb6291993c067f5b0d333f
John Edcel <johnedcelzenarosa@gmail.com>
2023-08-17 19:01:47 +0800

    list of territories updates

--- netzwelt-client/src/components/header/index.tsx
+++ netzwelt-client/src/components/header/index.tsx
@@ -15,7 +15,6 @@ export default function Header(){
     const logout = ()=>{
         localStorage.clear();
         setContext(null);
-
         router.push('/');        
     }
 
@@ -28,11 +27,11 @@ export default function Header(){
                     <img src="/quarta-logo-1.png" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Quarta</span>
                 </a>
-                {userInfo 
-                    &&
+                {(userInfo  || context?.userInfo) ? 
                     <div className="flex items-center lg:order-2">
-                        <a href="#" className="text-red-800 dark:text-white hover:bg-red-50 focus:ring-4 focus:ring-red-500 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-red-500 focus:outline-none dark:focus:ring-red-200" onClick={logout}>Log Out</a>                    
+                        <a href="/" className="text-red-800 dark:text-white hover:bg-red-50 focus:ring-4 focus:ring-red-500 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-red-500 focus:outline-none dark:focus:ring-red-200" onClick={logout}>Log Out</a>                    
                     </div>         
+                    : ''
                 }       
                            
             </div>
